233189d4e75c73637ee93d5b9e2e38adefacff1b
Jalen <jalenfreeman20@gmail.com>
2025-12-19 17:37:31 -0500

    Adding a "fleet" of cars

--- TrainSimulation/TrafficSim.cpp
+++ TrainSimulation/TrafficSim.cpp
@@ -56,7 +56,7 @@ public:
 		}
 		else {
 			// brake
-			if (velocity > 0) velocity -= 150.0f * dt;
+			if (velocity > 0) velocity -= 400.0f * dt;
 			if (velocity < 0) velocity = 0;
 		}
 
@@ -74,7 +74,9 @@ int main() {
 	window.setFramerateLimit(60.0f);
 
 	/*    2. CREATE SIM OBJECTS    */
-	Car myCar(50.0f, 300.0f);
+	// Car myCar(50.0f, 300.0f);
+	std::vector<Car> fleet;
+	float spawnTimer = 0.0f;
 	TraficLight light(600.f, 250.f);
 
 	sf::Clock clock;
@@ -92,17 +94,43 @@ int main() {
 		sf::Time elapsed = clock.restart();
 		float dt = elapsed.asSeconds();
 
-		// logic: is the car close to a red light?
-		bool isSafeToDrive{ true };
-		if (light.state == 0 && myCar.x < light.shape.getPosition().x && (light.shape.getPosition().x - myCar.x) < 200) {
-			isSafeToDrive = false;
+		// logic: spawner
+		spawnTimer += dt;
+		if (spawnTimer > 5.0f) {	// spawn ever 'x' seconds
+			if (fleet.empty() || fleet.back().x > 50.0f) {
+				fleet.push_back(Car(-50.f, 300.f));
+				spawnTimer = 0;
+			}
 		}
 
-		// update light and car
+		// update traffic light (independant of cars)
 		light.update(dt);
-		myCar.update(dt, isSafeToDrive);
 
-		// render the udpate
+
+		// logic: fleet
+		for (size_t i = 0; i < fleet.size(); i++) {
+			float limitLineX = light.shape.getPosition().x;
+			bool mustStop = false;
+
+			if (light.state == 0) {
+				if (fleet[i].x < limitLineX && (limitLineX - fleet[i].x) < 200) {
+					mustStop = true;
+				}
+			}
+
+			if (i > 0) {
+				Car& leader = fleet[i - 1];
+				float distanceToLeader = leader.x - fleet[i].x;
+
+				if (distanceToLeader < 100.0f && distanceToLeader > 0) {
+					mustStop = true;
+				}
+			}
+			// update this specific car
+			fleet[i].update(dt, !mustStop);
+		}
+
+		/*    RENDER SECTION    */
 		window.clear(sf::Color(50, 50, 50));
 
 		// draw road
@@ -111,9 +139,13 @@ int main() {
 		roadStrip.setFillColor(sf::Color(30, 30, 30));
 		window.draw(roadStrip);
 
-		// draw objects
+		// draw light
 		window.draw(light.shape);
-		window.draw(myCar.shape);
+
+		// draw fleet
+		for (Car& car : fleet) {
+			window.draw(car.shape);
+		}
 
 		window.display();
 
